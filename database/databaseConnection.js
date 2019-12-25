5ee23bc218caefd4bbfacf6834a9f3dbec313832
user <mahesworrajshrestha@gmail.com>
2019-12-25 15:49:14 +0545

    database connection has been created

---
+++ database/databaseConnection.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("maheswor_api", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  logging: false
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+module.exports = {
+  Sequelize,
+  sequelize
+};
