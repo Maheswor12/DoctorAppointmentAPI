196ab6e0ee25856692f51358d950b65871de48a2
user <mahesworrajshrestha@gmail.com>
2019-12-26 21:29:06 +0545

    registration and validation is done

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,33 +3,69 @@ const user = require("../model/user");
 exports.validator = (req, res, next) => {
   // to check wheather the username is empty or not
   if (req.body.fullname === "") {
-    res.send("username cannot be empty");
+    res.send("fullname cannot be empty");
   } else if (req.body.email === "") {
+    res.send("email cannot be empty");
+  } else if (req.body.number === "") {
+    res.send("number cannot be empty");
+  } else if (req.body.bloodGroup === "") {
+    res.send("bloodgroup cannot be empty");
+  } else if (req.body.age === "") {
+    res.send("age cannot be empty");
+  } else if (req.body.gender === "") {
+    res.send("gender cannot be empty");
+  } else if (req.body.password === "") {
+    res.send("password cannot be empty");
+  } else if (req.body.image === "") {
+    res.send("image cannot be empty");
   }
   user
     .findOne({
-      where: { fullname: req.body.fullname }
+      where: { email: req.body.email }
     })
     .then(function(result) {
       if (result === null) {
-        res.status(201);
-        res.json({
-          satus: 201,
-          message: "Yplease registered first"
-        });
-        console.log("registered first");
+        next();
       } else {
-        req.passwordFromDB = result.dataValues.password;
         res.status(409);
         res.json({
           satus: 409,
-          message: "Username exists"
+          message: "You are already registered"
         });
-        next();
       }
     })
-    .catch(function(err) {});
-  next();
+    .catch(function(err) {
+      next(err);
+    });
 };
 
-
+exports.registerUser = (req, res, next) => {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      //console.log(hash);
+      user
+        .create({
+          fullname: req.body.fullname,
+          email: req.body.email,
+          number: req.body.number,
+          bloodGroup: req.body.bloodGroup,
+          age: req.body.age,
+          gender: req.body.gender,
+          password: hash,
+          image: "mahesh_image"
+        })
+        .then(function(result) {
+          //console.log(result);
+          res.status(201);
+          res.json({
+            satus: 201,
+            message: "You have been registered successfully"
+          });
+        })
+        .catch(function(err) {
+          //console.log(err);
+          next(err);
+        });
+    });
+  });
+};
